5ee49fe62de10946ee833d7e45998713659185b4
AlexandreHome <alexandre.souzafilho@hotmail.com>
2026-02-09 14:34:23 -0300

    Sort, Shift e binarys

--- Estudo/Binary/Left_Right_Shit.py
+++ Estudo/Binary/Left_Right_Shit.py
@@ -21,7 +21,7 @@ Sempre que eu uso um shift é como se eu andasse para esquerda ou direita(depend
 5 << 1  -> Me diz q estou andando 1 casa binaria para a esquerda utilziando o binario de 5:
 
     32 16 8 4 2 1 
-> 0 0 0 0 1 0 1   - Na casa vazia é adicionado um  0, agora o 5 n é mais 5 e sim 10, ja que agora o 1 esta na casa de 8 e 2
+> 0 0 0 0 1 0 1 _ - Na casa vazia é adicionado um  0, agora o 5 n é mais 5 e sim 10, ja que agora o 1 esta na casa de 8 e 2
 
 Regra geral para facilitar entendimento:
 
@@ -36,3 +36,4 @@ Regra geral para facilitar entendimento:
 """
 
 5 << 1  # 10
+print(5 << 1)
---
+++ Estudo/Sorting/MergeSort.py
@@ -0,0 +1,12 @@
+"""
+Normalmente usado para sorting em linked list
+
+complexidade de tempo: O(n log n)
+
+complexidade espaço: O(n)
+
+
+
+"""
+
+
