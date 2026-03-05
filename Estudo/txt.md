255bf64cb0075417af395134b71a5f3e8eb26cde
AlexandreHome <alexandre.souzafilho@hotmail.com>
2026-03-04 21:54:53 -0300

    revisão

---
+++ Estudo/txt.md
@@ -0,0 +1,23 @@
+
+BIG O notation:
+
+- mede a complexidade de tempo e espaço de um algoritmo, tempo de execução e espaço na memória.
+
+- importante para escrever códigos eficientes sem grande impacto( e tbm para ajduar em leetcodes/entrevistas)
+
+Complexidades mais importantes:
+- O(1)
+- O(n)
+- O(Log n)
+- O(n log n)
+- O(n²)
+
+
+
+Regra prática:
+
+- Loop simples → O(n)
+- Loop dentro de loop → O(n²)
+- Dividindo problema pela metade → O(log n)
+- Recursão com múltiplas chamadas → geralmente exponencial
+
--- Exercícios/Sliding_window.py
+++ Exercícios/Sliding_window.py
@@ -1,19 +1,3 @@
-from collections import Counter
-
-class Solution:
-    def maximumLengthSubstring(self, s: str) -> int:
-        count = Counter()
-        n = len(s)
-        left = 0
-        res = 0
-        for right in range(n):
-            count[s[right]] += 1
-            while count[s[right]] > 2:
-                count[s[left]] -= 1
-                left += 1
-            res = max(res, right - left + 1)
-        return res
-    
 
 """
 count = Counter()  # Dicionário para contar frequência de caracteres
@@ -32,3 +16,20 @@ for right in range(n):
 
 
 """
+
+from collections import Counter
+
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        count = Counter() # Counter("aab") → {'a': 2, 'b': 1}
+        n = len(s)
+        left = 0
+        res = 0
+        for right in range(n):
+            count[s[right]] += 1
+            while count[s[right]] > 2:
+                count[s[left]] -= 1
+                left += 1
+            res = max(res, right - left + 1)
+        return res
+    
